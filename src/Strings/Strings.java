ca8f1b6ff7ffbff96b3cf94c54f6ee05895a63af
xabertum <mohatar@gmail.com>
2018-06-16 13:02:30 +0200

    added example

---
+++ src/Strings/Strings.java
@@ -0,0 +1,20 @@
+package Strings;
+
+public class Strings {
+
+    public static void problema2() {
+	
+	
+	
+	
+    }
+    
+    
+    
+    
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+    }
+
+}
