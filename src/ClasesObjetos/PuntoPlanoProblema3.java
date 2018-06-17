21f2f989cce2c01372d577222b12b65968268ffd
xabertum <mohatar@gmail.com>
2018-06-17 13:01:56 +0200

    added example

---
+++ src/ClasesObjetos/PuntoPlanoProblema3.java
@@ -0,0 +1,31 @@
+package ClasesObjetos;
+
+import java.util.Scanner;
+
+public class PuntoPlanoProblema3 {
+
+    private Scanner scanner;
+    private int x, y;
+    
+    
+    public void inicializar() {
+	
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public static void main(String[] args) {
+	
+	
+	
+	
+
+    }
+
+}
