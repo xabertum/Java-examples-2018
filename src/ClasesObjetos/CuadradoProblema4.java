3d4bb4ce5d975bea480a842f4acc3151648dda21
xabertum <mohatar@gmail.com>
2018-06-17 16:32:33 +0200

    added example

--- src/ClasesObjetos/CuadradoProblema4.java
+++ src/ClasesObjetos/CuadradoProblema4.java
@@ -1,10 +1,30 @@
 package ClasesObjetos;
 
+import java.util.Scanner;
+
 public class CuadradoProblema4 {
 
+    private Scanner scanner;
+    private int lado;
+    
+    
+    public void inicializar() {
+	scanner = new Scanner(System.in);
+	System.out.println("introduce el valor de lado: ");
+	lado = scanner.nextInt();
+	
+    }
+    
+    public void imprimir() {
+	System.out.println("Perimetro: " + lado*4);
+	System.out.println("Superficie: " + lado*2);
+    }
+    
+    
     public static void main(String[] args) {
-	// TODO Auto-generated method stub
-
+	CuadradoProblema4 cuadradoProblema4 = new CuadradoProblema4();
+	cuadradoProblema4.inicializar();
+	cuadradoProblema4.imprimir();
     }
 
 }
