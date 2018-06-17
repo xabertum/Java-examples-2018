c151ffe656044beacbc8f459fcc7e1b8e48db83b
xabertum <mohatar@gmail.com>
2018-06-17 13:10:44 +0200

    Added example

--- src/ClasesObjetos/PuntoPlanoProblema3.java
+++ src/ClasesObjetos/PuntoPlanoProblema3.java
@@ -6,24 +6,42 @@ public class PuntoPlanoProblema3 {
 
     private Scanner scanner;
     private int x, y;
-    
-    
+
     public void inicializar() {
-	
+	scanner = new Scanner(System.in);
+
+	System.out.println("Introduce el punto x:");
+	x = scanner.nextInt();
+
+	System.out.println("Introduce el punto y: ");
+	y = scanner.nextInt();
+
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+    public void imprimirCuadrante() {
+	if (x > 0 && y > 0)
+	    System.out.println("Primer cuadrante");
+	else {
+	    if (x < 0 && y > 0)
+		System.out.println("sengudo cuadrante");
+	    else {
+		if (x < 0 && y < 0)
+		    System.out.println("Tercer Cuadrante");
+		else {
+		    if (x > 0 && y < 0)
+			System.out.println("Cuarto Cuadrante");
+		    else
+			System.out.println("datos erroneos");
+		}
+	    }
+	}
+
+    }
+
     public static void main(String[] args) {
-	
-	
-	
+	PuntoPlanoProblema3 puntoPlanoProblema3 = new PuntoPlanoProblema3();
+	puntoPlanoProblema3.inicializar();
+	puntoPlanoProblema3.imprimirCuadrante();
 	
 
     }
