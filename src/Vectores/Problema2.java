9115c30c793b498b5ebf19fc72c240bd36eab8c4
xabertum <mohatar@gmail.com>
2018-06-19 12:15:12 +0200

    Added example

--- src/Vectores/Problema2.java
+++ src/Vectores/Problema2.java
@@ -5,13 +5,40 @@ import java.util.Scanner;
 public class Problema2 {
 
     private Scanner scanner;
-    private int[] altura;
+    private float[] altura;
+    private float promedio;
     
     
     public void inicializarVector() {
+	scanner = new Scanner(System.in);
+	altura = new float[5];
 	
+	for (int i = 0; i < altura.length; i++) {
+	    System.out.println("Introduce la altura " + i + ":" );
+	    altura[i] = scanner.nextInt();
+	}	
+    }
+    
+    public void media() {
+	float suma = 0;	
+	for (int i = 0; i < altura.length; i++) {
+	   suma += altura[i];	   
+	}
 	
+	promedio = suma/altura.length;
+    }
+    
+    public void MayoresMenores () {
+	int mayor = 0;
+	int menor = 0;
 	
+	for (int i = 0; i < altura.length; i++) {
+	    if (altura[i] > promedio) mayor++;
+	    else menor++;
+	}
+	
+	System.out.println("Mayores que el promedio: " + mayor);
+	System.out.println("Menores que el promedio: " + menor);	
     }
     
     
