2740dc21f21ff830776702aac5286186ef04a871
xabertum <mohatar@gmail.com>
2018-06-19 12:03:25 +0200

    added example

---
+++ src/Vectores/Problema2.java
@@ -0,0 +1,24 @@
+package Vectores;
+
+import java.util.Scanner;
+
+public class Problema2 {
+
+    private Scanner scanner;
+    private int[] altura;
+    
+    
+    public void inicializarVector() {
+	
+	
+	
+    }
+    
+    
+    
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+    }
+
+}
