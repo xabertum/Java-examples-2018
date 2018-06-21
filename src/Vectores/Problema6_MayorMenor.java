ad837fa6be90ff6dc1efc08d668d63a71ac047a2
xabertum <mohatar@gmail.com>
2018-06-21 20:31:09 +0200

    added example

---
+++ src/Vectores/Problema6_MayorMenor.java
@@ -0,0 +1,55 @@
+package Vectores;
+
+import java.util.Scanner;
+
+public class Problema6_MayorMenor {
+
+    private Scanner scanner;
+    private int numElementos;
+    private int[] vector;
+
+    public void inicializarVector() {
+
+	scanner = new Scanner(System.in);
+	System.out.println("Cuantos elementos tendrá el vector: ");
+	numElementos = scanner.nextInt();
+	vector = new int[numElementos];
+
+	for (int i = 0; i < vector.length; i++) {
+	    System.out.println("Valor de la posición " + i + " :");
+	    vector[i] = scanner.nextInt();
+	}
+    }
+
+    public int menorValor() {
+
+	int menor = vector[0];
+	for (int i = 0; i < vector.length; i++) {
+
+	    if (vector[i] < menor) {
+		menor = vector[i];
+	    }
+	}
+
+	return menor;
+    }
+
+    public boolean seRepite() {
+
+	boolean seRepite = false;
+
+	for (int i = 0; i < vector.length; i++) {
+	    if (vector[i] == vector[i + 1])
+		seRepite = true;
+
+	}
+
+	return seRepite;
+    }
+
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+    }
+
+}
