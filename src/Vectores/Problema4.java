5de70e98c0a4661686919c1f1bb7db46facb1e2d
xabertum <mohatar@gmail.com>
2018-06-21 15:04:46 +0200

    added example

--- src/Vectores/Problema4.java
+++ src/Vectores/Problema4.java
@@ -16,20 +16,24 @@ public class Problema4 {
 	vector = new int [n];
 	
 	for (int i = 0; i < vector.length; i++) {
-	    System.out.println("Ingrese posicion " + i +" :");
+	    System.out.println("Ingrese posicion " + i +": ");
 	    vector[i] = scanner.nextInt();
 	}
 	
     }
     
     public void imprimirVector() {
-	
-	
+	for (int i = 0; i < vector.length; i++) {
+	    System.out.println("Posicion " + i + ": " + vector[i]);
+	}
+		
     }
     
     
     public static void main(String[] args) {
-	
+	Problema4 problema4 = new Problema4();
+	problema4.cargarVector();
+	problema4.imprimirVector();
 
     }
 
