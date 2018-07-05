7f0b9b356720c1e6d3dee83c2dbf76a06a412230
xabertum <mohatar@gmail.com>
2018-07-05 16:04:37 +0200

    added example

--- src/Matrices/Problema2.java
+++ src/Matrices/Problema2.java
@@ -1,9 +1,39 @@
 package Matrices;
 
+import java.util.Iterator;
+import java.util.Scanner;
+
 public class Problema2 {
 
+    private Scanner scanner;
+    private int [][] matriz;
+    
+    
+    public void inicializarMatriz() {
+	scanner = new Scanner(System.in);
+	matriz = new int [4][4];
+	
+	for (int i = 0; i < matriz.length; i++) {
+	    for (int j = 0; j < matriz.length; j++) {
+		System.out.println("ingrese componente:");
+		matriz[i][j] = scanner.nextInt();
+	    }
+	}	
+    }
+    
+    public void imprimirDiagonalPrincipal () {
+	for (int i = 0; i < matriz.length; i++) {
+	    System.out.println(matriz[i][i] +  " ");
+	}
+    }
+       
+    
+    
     public static void main(String[] args) {
-	// TODO Auto-generated method stub
+	
+	Problema2 matriz = new Problema2();
+	matriz.inicializarMatriz();
+	matriz.imprimirDiagonalPrincipal();
 
     }
 
