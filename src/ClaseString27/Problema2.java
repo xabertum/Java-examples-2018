d0b4e6bf10412b00ea936ed0ff1556441a610d1e
xabertum <mohatar@gmail.com>
2018-07-25 22:26:47 +0200

    added example

--- src/ClaseString27/Problema2.java
+++ src/ClaseString27/Problema2.java
@@ -1,9 +1,41 @@
 package ClaseString27;
 
+import java.awt.event.HierarchyBoundsAdapter;
+import java.util.Scanner;
+
 public class Problema2 {
 
+    private Scanner scanner;
+    private String mail;
+    
+    /**
+     * Constructor
+     */
+    public Problema2 () {
+	scanner = new Scanner(System.in);
+	
+	System.out.println("Ingrese la direccion: ");
+	mail = scanner.nextLine();
+		
+	
+    }
+    
+    public void comprobarArroba () {
+	boolean tieneArroba = false;
+	for (int i = 0; i < mail.length(); i++) {
+	    if (mail.charAt(i) == '@') tieneArroba = true;
+	}
+	
+	if (tieneArroba) System.out.println("la direccion contiene la arroba.");
+	else System.out.println("No hay arroba");
+	
+    }
+    
+    
     public static void main(String[] args) {
-	// TODO Auto-generated method stub
+	Problema2 problema2 = new Problema2();
+	problema2.comprobarArroba();
+	
 
     }
 
